430d285788c074967af2e72c92bf171361ecdf6c
realtechnerd <send.nithil@gmail.com>
2020-08-23 16:47:59 -0400

    Simple text fix.

--- script.js
+++ script.js
@@ -3,15 +3,20 @@ function calculator() {
     const firstnum = prompt("First number.");
     const secondnum = prompt("Second number.");
     const operator = prompt("Select operator. Your choices are '+','-','/', and '*'");
+    let output;
 
-    if (operator === "+") { 
-        display.innerHTML = parseInt(firstnum) + parseInt(secondnum);
+    if (operator === "+") {
+        output = parseInt(firstnum) + parseInt(secondnum);
+        display.innerHTML = "The answer is: " + output + ".";
     } else if (operator === '-') {
-        display.innerHTML = parseInt(firstnum) - parseInt(secondnum);
+        output = parseInt(firstnum) - parseInt(secondnum);
+        display.innerHTML = "The answer is: " + output + ".";
     } else if (operator === '/') {
-        display.innerHTML = parseInt(firstnum) / parseInt(secondnum);
+        output = parseInt(firstnum) / parseInt(secondnum);
+        display.innerHTML = "The answer is: " + output + ".";
     } else if (operator === '*') {
-        display.innerHTML = parseInt(firstnum) * parseInt(secondnum);
+        output = parseInt(firstnum) * parseInt(secondnum);
+        display.innerHTML = "The answer is: " + output + ".";
     } else {
         display.innerHTML = "Input missing or wrong input."
     }
